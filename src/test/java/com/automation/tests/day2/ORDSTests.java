3755fad86fe3a595680fd6e5509270b7934a7764
Ash0218 <ajykim218@gmail.com>
2020-02-04 16:39:17 -0500

    Created ORDS Tests class

--- src/test/java/com/automation/tests/day2/ORDSTests.java
+++ src/test/java/com/automation/tests/day2/ORDSTests.java
@@ -1,4 +1,16 @@
 package com.automation.tests.day2; // 012320
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*; // 3
+import static org.junit.jupiter.api.Assertions.*; // 4
+
 public class ORDSTests {
+
+    @Test // 2
+    public void test1(){ // 1
+        // run to check if it's working
+
+
+    }
 }
