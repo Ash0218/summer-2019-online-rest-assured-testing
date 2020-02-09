914ccf72c83ab2ec7483f56742aff6bdbdfb7776
Ash0218 <ajykim218@gmail.com>
2020-02-09 14:48:14 -0500

    Created ORDS Tests class

--- src/test/java/com/automation/tests/day2/ORDSTests.java
+++ src/test/java/com/automation/tests/day2/ORDSTests.java
@@ -1,6 +1,7 @@
 package com.automation.tests.day2; // 012320
 
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*; // 3
@@ -14,8 +15,20 @@ public class ORDSTests {
     //  we were accessing data base directly. Now, we're gonna
     //  access web service.
 
+    // According to OOP conventions, all instance variable should be private
+    //  but, if we will make it public, it will be make any difference for us.
+    // It's just good practice, so later, we will not hesitate which keyword to use
+    //  when it's gonna be important
+
     @Test // 2
     // verify that status code is 200
+    // We start from "given()", then we can specify what type of request
+    //  like: get(), put(), delete(), post()... And as a parameter, we
+    //  enter resource location (URI). Then, we are getting response back.
+    //  That response we can put into Response object. From response
+    //  object, we can retrieve: body, header, status code.
+    // -> it works without RestAssured() b.c of
+    //  static import.
     public void test1(){ // 1
         // run to check if it's working
 
@@ -28,6 +41,49 @@ public class ORDSTests {
         System.out.println(response.prettyPrint()); // 9
         // it makes the jason response pretty (easy to see)
 
+    }
+
+    // Task: get employee with id 100 and verify that status code is 200
+    //  Also, print body
+    @Test // 10
+    public void test2(){ // 11
+        Response response = given().
+                header("Accept", "application/json").
+                // header stands for meta data. Usually, it is used to
+                //  include cookies. In this example, we are specifying what
+                //  kind of response type we need b.c web service can return
+                //  json or xml when we put header info "Accept", "application/json".
+                //  We are saying that we need only json as response.
+                // You can use "accept" instead of "Accept"
+                get(baseURI+"/employees/100"); // 12
+                 // employee in ID 100 -> path parameter.
+
+        int actualStatusCode = response.getStatusCode(); // 13
+        System.out.println(response.prettyPrint()); // 15
+        assertEquals(200, actualStatusCode); // 14
+        // it must return 200 (code of success) or it fails
+
+        // get information about response content type, you can
+        //  retrieve from response object.
+        System.out.println("What kind of content server sends to you, in this response: "
+                + response.getHeader("Content-Type")); // 15
+        // it shows: What kind of content server sends to you, in this response:
+        //  application/json
 
     }
+
+    // Task: Perform GET request to /regions, print body and all headers.
+    @Test
+    public void test3(){ // 16
+        Response response = given().get(baseURI+"/regions"); // 17
+        assertEquals(200, response.getStatusCode()); // 18
+        // to get specific header:
+        Header header = response.getHeaders().get("Content-Type"); // 19
+        // Print all headers one by one:
+        for (Header h: response.getHeaders()){ // 20
+            System.out.println(h); // 21
+        }
+        System.out.println(response.prettyPrint()); // 22
+    }
+
 }
