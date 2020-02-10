d533a25834d95598b023c42b596a8781c6fea20c
Ash0218 <ajykim218@gmail.com>
2020-02-10 16:59:55 -0500

    Meta weather tests

--- src/test/java/com/automation/tests/day2/MetaWeatherTests.java
+++ src/test/java/com/automation/tests/day2/MetaWeatherTests.java
@@ -11,4 +11,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MetaWeatherTests {
 
+    /*
+    /api/location/search/?query=san
+    /api/location/search/?query=london
+    /api/location/search/?lattlong=36.96,-122.02
+    /api/location/search/?lattlong=50.068,-5.316
+     */
+    private String baseURI = "https://www.metaweather.com/api/"; // 1
+
+    @Test
+    public void test1(){ // 2
+        Response response = given()
+                .baseUri(baseURI+"/location/search/")
+                .queryParam("query", "san")
+                .get(); // 3
+        assertEquals(200, response.getStatusCode()); // 4
+        System.out.println(response.prettyPrint()); // 5
+
+    }
 }
