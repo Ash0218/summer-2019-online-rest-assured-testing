1423d09b2f172b29065f4516a8e1347a3d3768e9
Ash0218 <ajykim218@gmail.com>
2020-02-24 14:36:53 -0500

    day5

--- src/test/java/com/automation/tests/day5/ORDSTestsDay5.java
+++ src/test/java/com/automation/tests/day5/ORDSTestsDay5.java
@@ -32,5 +32,27 @@ public class ORDSTestsDay5 { // 1
         then user verifies that status code is 200
         and user verifies that average salary is greater than 5000
      */
+    @Test
+    @DisplayName("verify that average salary is greater than 5000")
+    public void test1(){ // 3
+        Response response = given().
+                                    accept(ContentType.JSON).
+                            when().
+                                    get("/employees"); // 4
 
+        JsonPath jsonPath = response.jsonPath(); // 5
+
+        List<Integer> salaries = jsonPath.getList("items.salary"); // 6
+
+        int sum = 0; // 7
+        for (int salary : salaries) { // 8
+            sum += salary; // 9
+        } // we are finding a sum of all salaries
+
+        int avg = sum / salaries.size(); // 10
+        // we are calculating average: salary sum/ count
+
+        assertTrue(avg > 5000, "ERROR: actually average salary is lower than 5000: "+avg); // 11
+        // we are asserting that average salary is greater than 5000
+    }
 }
