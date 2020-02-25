1b3e2ef79f1eae62e3bf612f149746a489782750
Ash0218 <ajykim218@gmail.com>
2020-02-24 22:20:06 -0500

    day5-POJOS

--- src/test/java/com/automation/pojos/Job.java
+++ src/test/java/com/automation/pojos/Job.java
@@ -1,14 +1,20 @@
 package com.automation.pojos; // 012820
 
+import com.google.gson.annotations.SerializedName;
+
 public class Job { // 1
-    private String job_id; // 2
+    @SerializedName("job_id")
+    // @SerializedName helps to get the real data from the source (here, Postman)
+    //  #2 jobId is wrong -> it should be "job_id" like the Postman, so @SerializedName
+    //  helps to figure out.
+    private String jobId; // 2
     private String job_title; // 3
     private Integer min_salary; // 4
     private Integer max_salary; // 5
 
     public Job(String job_id, String job_title, Integer min_salary, Integer max_salary) { // 6
         // right click on empty space, generate, constructor, select all parameters, ok
-        this.job_id = job_id; // 7
+        this.jobId = job_id; // 7
         this.job_title = job_title; // 8
         this.min_salary = min_salary; // 9
         this.max_salary = max_salary; // 10
@@ -16,11 +22,11 @@ public class Job { // 1
 
     public String getJob_id() {  // 11
         // right click on empty space, Generate, Getter and Setter, select all, ok
-        return job_id; // 12
+        return jobId; // 12
     }
 
     public void setJob_id(String job_id) { // 13
-        this.job_id = job_id; // 14
+        this.jobId = job_id; // 14
     }
 
     public String getJob_title() { // 15
@@ -52,7 +58,7 @@ public class Job { // 1
     public String toString() { // 27
         // right click on empty space, generate, toString(), ok
         return "Job{" +
-                "job_id='" + job_id + '\'' +
+                "job_id='" + jobId + '\'' +
                 ", job_title='" + job_title + '\'' +
                 ", min_salary=" + min_salary +
                 ", max_salary=" + max_salary +
