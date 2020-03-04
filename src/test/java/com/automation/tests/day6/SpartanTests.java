3fc8dc12c0933e87271d5d703db21df895077045
Ash0218 <ajykim218@gmail.com>
2020-03-04 14:44:49 -0500

    day 6

--- src/test/java/com/automation/tests/day6/SpartanTests.java
+++ src/test/java/com/automation/tests/day6/SpartanTests.java
@@ -1,4 +1,27 @@
 package com.automation.tests.day6; // 013020
 
+import com.automation.utilities.ConfigurationReader;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.BeforeAll;
+import io.restassured.http.Header;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.*;
+
 public class SpartanTests {
+
+    @BeforeAll
+    public void setup(){ // 1
+        baseURI = ConfigurationReader.getProperty("spartan.uri"); // 2
+    }
 }
