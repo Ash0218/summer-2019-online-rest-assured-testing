8875285a539096b85aeba91da1deae4d6e0b161d
Ash0218 <ajykim218@gmail.com>
2020-04-20 14:01:13 -0400

    day8

--- src/test/java/com/automation/tests/day8/BadSSLTest.java
+++ src/test/java/com/automation/tests/day8/BadSSLTest.java
@@ -32,9 +32,32 @@ import static org.hamcrest.Matchers.*;
 public class BadSSLTest {
     @BeforeAll
     public static void before(){ // 1
-        baseURI = "https://untrusted-root.badssl.com/";
-    } // 2
-    public void test1(){ // 1
+        baseURI = "https://untrusted-root.badssl.com/"; // 2
+    }
+
+    @Test
+    @DisplayName("Access web site with bad SSL")
+    public void test1(){ // 3
+        Response response = get().prettyPeek(); // 4
+        // it shows: javax.net.ssl.SSLHandshakeException -> b.c there is no
+        //  SSL certificate, there is no handshake. If web site cannot provide
+        //  valid certificate, secured connection is not possible. Client will
+        //  reject to exchange information by default.
+
+        System.out.println(response.statusCode()); // 5
+        // there is no status code b.c there was no exchange of information at all.
+    }
+
+
+    @Test
+    @DisplayName("Access web site with bad SSL (solution)")
+    public void test2(){ // 6
+        Response response = given().relaxedHTTPSValidation().get().prettyPeek(); // 7
+        // relaxedHTTPSValidation() -> ignores SSL issues
+
+        System.out.println(response.statusCode()); // 8
+
+        assertEquals(200, response.getStatusCode()); // 9
 
     }
 }
