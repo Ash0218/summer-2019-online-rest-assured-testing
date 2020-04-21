03c59b058b33dee6f7251bf709aaed3da63be124
Ash0218 <ajykim218@gmail.com>
2020-04-21 11:05:42 -0400

    day8

--- src/test/java/com/automation/tests/day8/BearerTokenTestsWithBookit.java
+++ src/test/java/com/automation/tests/day8/BearerTokenTestsWithBookit.java
@@ -35,4 +35,22 @@ public class BearerTokenTestsWithBookit {
     public static void setup(){ // 1
         baseURI = ConfigurationReader.getProperty("bookit.qa1"); // 2
     }
+
+
+    // Let's get the list of all rooms and verify that status code is 200
+
+
+    public String getToken(){ // 3
+        ////https://cybertek-reservation-api-qa.herokuapp.com/sign?email=vasyl@cybertekschool.com&password=cybertek2020
+        Response response = given().
+                                queryParam("email", ConfigurationReader.getProperty("team.leader.email")).
+                                // "team.leader.email" is from configuration.properties
+                                queryParam("password", ConfigurationReader.getProperty("team.leader.password")).
+                            when().
+                                get("/sign").prettyPeek(); // 4
+
+        return response.jsonPath().getString("accessToken"); // 5
+        // we have to add token every time
+
+    }
 }
